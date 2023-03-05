ab59ac75b4268da072542bd8a7ed64b7b3f231bc
Joe Pea <joe@trusktr.io>
2023-03-04 20:24:57 -0800

    format grammar code, update example

--- examples/basic/main.ts
+++ examples/basic/main.ts
@@ -1,39 +1,85 @@
+// When running debugger (in VS Code hit F5, or hit the play button in "Run and
+// Debug" in the sidebar, or run the "Debug: Start Debugging" command), in the
+// new VS Code window (which is now running our extension) you can paste this
+// code to see the errors with squiggly underlines, type checking, and
+// intellisense (i.e. tooltips when hovering on code or errors).
+
 /////////////////////////////////////////////////////////
-// Example of good code ////////////////////////////////
+// Testing Code ////////////////////////////////////////
 
-let Foo = 123
-
-function bar() {}
-
-function doSomething(foo) {
+function foo(foo: i32): f64 {
 	/**
 	 * This is a var
 	 */
-	let Foo = 4.56
+	let foo = 456
+
+	return 1.2;
+	return 1;
+	return (1 + 4);
+	return (Foo() * bar());
+
+	return
+	(foo() * bar()); // should be an unreachable code error
 }
 
 /**
  * Hello *there*
  */
-let Asdf = 456
+var a = 456
+const b: i32 = 456
+let c = 1.0;
+let d: f64 = 1.0
 
-let Rock = 1
-const Car = 2
-var Boat = 3
+// type error
+let e: f64 = 1
+let f: i32 = 456.0;
 
-let Func1 = function (foo) {
-	let Foo = 456
-}
+let rock = 1
+const car = 2
+var boat = 3;
 
-doSomething(Foo, 1.2)
-
-/////////////////////////////////////////////////////////
-// Example of bad code /////////////////////////////////
-
-// const d Car = 2
-
-// doSomething(foo) {
-// 	let Foo = 456
+// Not supported yet (we will need to figure out how the Wasm output will handle function expressions).
+// let func = function (foo) {
+//   let foo = 456
 // }
 
-// function () {}
+export function foo(foo: i32): f64 {
+	/**
+	 * This is a var
+	 */
+	let bar = 456
+
+	return 2.5 + foo * bar();
+}
+
+foo()
+foo(123, 1.2);
+foo (a, b, c)
+foo;
+() // error, expect expression inside parens
+
+// (a, b, c); // comma-separated expressions in parens not handled yet
+
+function bar(a: f64, b: f64): f64 {
+	return a * b;
+	(); // error, expect expression inside parens
+}
+
+function baz(a: f64, b: f64): f64 {
+	return a * b
+	(); // ok, parens on new line
+}
+
+// error missing return type annotation (no inference for now)
+function baz(a: f64, b: f64) { }
+
+// error missing parameter type annotation (no callsite generic inference yet)
+function baz(a: f64, b): i32 { }
+
+function () {} // error 
+
+// totally bad top level syntax
+const d Car = 2
+doSomething(foo) { // this will be ok in a class body
+	let Foo = 456
+}
