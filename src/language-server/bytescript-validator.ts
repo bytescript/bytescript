7db961dc7e8d8ddd3e926e59a33edc7b219dd53d
Jairus Tanaka <jairus.v.tanaka@outlook.com>
2023-03-10 21:53:43 -0800

    format code

--- src/language-server/bytescript-validator.ts
+++ src/language-server/bytescript-validator.ts
@@ -1,4 +1,4 @@
-import type {AstNode, ValidationAcceptor, ValidationChecks} from 'langium'
+import type {AstNode, ValidationAcceptor, ValidationChecks} from "langium";
 import {
 	ByteScriptAstType,
 	ClassicFunction,
@@ -6,24 +6,24 @@ import {
 	CallExpression,
 	isIdentifier,
 	InvalidParenthesis,
-} from './generated/ast'
-import type {ByteScriptServices} from './bytescript-module'
-import {getType, isAssignable} from './types/types'
-import {isErrorType, TypeDescription, typeToString} from './types/descriptions'
+} from "./generated/ast";
+import type {ByteScriptServices} from "./bytescript-module";
+import {getType, isAssignable} from "./types/types";
+import {isErrorType, TypeDescription, typeToString} from "./types/descriptions";
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: ByteScriptServices) {
-	const registry = services.validation.ValidationRegistry
-	const validator = services.validation.ByteScriptValidator
+	const registry = services.validation.ValidationRegistry;
+	const validator = services.validation.ByteScriptValidator;
 	const checks: ValidationChecks<ByteScriptAstType> = {
 		VariableDeclaration: validator.checkVarDeclaration,
 		ClassicFunction: validator.checkClassicFunction,
 		CallExpression: validator.checkCallExpression,
 		InvalidParenthesis: validator.checkInvalidParenthesis,
-	}
-	registry.register(checks, validator)
+	};
+	registry.register(checks, validator);
 }
 
 /**
@@ -31,61 +31,61 @@ export function registerValidationChecks(services: ByteScriptServices) {
  */
 export class ByteScriptValidator {
 	checkVarDeclaration(varDecl: VariableDeclaration, accept: ValidationAcceptor): void {
-		const declType = getType(varDecl)
+		const declType = getType(varDecl);
 
-		if (acceptErrorType(varDecl, declType, accept)) return
+		if (acceptErrorType(varDecl, declType, accept)) return;
 
 		if (varDecl.type || varDecl.value) {
 			if (varDecl.value) {
-				const valueType = getType(varDecl.value)
+				const valueType = getType(varDecl.value);
 
-				if (acceptErrorType(varDecl.value, valueType, accept)) return
+				if (acceptErrorType(varDecl.value, valueType, accept)) return;
 
 				if (varDecl.type) {
 					if (!isAssignable(valueType, declType)) {
 						accept(
-							'error',
+							"error",
 							`Type '${typeToString(valueType)}' is not assignable to type '${typeToString(declType)}'.`,
-							{node: varDecl, property: 'value'},
-						)
+							{node: varDecl, property: "value"},
+						);
 					}
 				} else {
 					// Value is assignable because it was used to infer the declaration type.
 				}
 			}
 		} else if (!varDecl.type && !varDecl.value) {
-			accept('error', 'Variable declarations require a type annotation and an assigned value', {
+			accept("error", "Variable declarations require a type annotation and an assigned value", {
 				node: varDecl,
-				property: 'name',
-			})
+				property: "name",
+			});
 		}
 	}
 
 	checkClassicFunction(func: ClassicFunction, accept: ValidationAcceptor): void {
-		const type = getType(func)
-		if (acceptErrorType(func, type, accept)) return
+		const type = getType(func);
+		if (acceptErrorType(func, type, accept)) return;
 	}
 
 	checkCallExpression(call: CallExpression, accept: ValidationAcceptor) {
 		if (isIdentifier(call.callee)) {
-			const identifier = call.callee
-			const char = identifier.value.charAt(0)
+			const identifier = call.callee;
+			const char = identifier.value.charAt(0);
 			if (char.toLowerCase() !== char) {
-				accept('error', 'FunctionCall: Expected lower case name.', {node: call, property: 'callee'})
+				accept("error", "FunctionCall: Expected lower case name.", {node: call, property: "callee"});
 			}
 		}
 	}
 
 	checkInvalidParenthesis(expression: InvalidParenthesis, accept: ValidationAcceptor) {
-		accept('error', 'SyntaxError: Expected expression.', {node: expression})
+		accept("error", "SyntaxError: Expected expression.", {node: expression});
 	}
 }
 
 function acceptErrorType(node: AstNode, type: TypeDescription, accept: ValidationAcceptor): boolean {
 	if (isErrorType(type)) {
-		accept('error', type.message, {node})
-		return true
+		accept("error", type.message, {node});
+		return true;
 	}
 
-	return false
+	return false;
 }
