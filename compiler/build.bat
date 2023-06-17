33655bcd5bd4114266d732d32fe2a47b2146d76c
Kara Rawson <rawsonkara@gmail.com>
2023-06-17 08:05:51 -0400

    added color cli palette and add better output to the build bat file

--- compiler/build.bat
+++ compiler/build.bat
@@ -5,10 +5,6 @@ REM author: @zoedreams
 REM date: 230617
 REM description: used to build the compiler called `bsc.exe` using CMake
 
-@echo.
-@echo Building bsc.exe with CMake...
-@echo.
-
 REM Check to see if we pass in the debug switch
 
 @if "%1" == "" (
@@ -16,6 +12,10 @@ REM Check to see if we pass in the debug switch
 ) else (
   @set config=%1
 )
+
+@echo.
+@echo   [34mBuilding `bsc` (%config%) with [01mCMake[0m...[0m
+@echo.
 
 REM Check to see if the config value is valid
 
@@ -29,10 +29,12 @@ REM Check to see if the config value is valid
 REM The first command (@cmake -S . -B ./build) is the configure step, 
 REM where cmake generates the build system for the project
 
+@set status=0
+
 @cmake -S . -B .
 IF %ERRORLEVEL% NEQ 0 (
   ECHO CMake failed with error code %ERRORLEVEL%
-  EXIT /B %ERRORLEVEL%
+  @set status=1
 )
 
 REM The second command (@cmake --build ./build --config %config%) is the 
@@ -41,10 +43,14 @@ REM build step, where cmake compiles the project into `./bin/%config%/bsc.exe`
 @cmake --build . --config %config%
 IF %ERRORLEVEL% NEQ 0 (
   ECHO CMake failed with error code %ERRORLEVEL%
-  EXIT /B %ERRORLEVEL%
+  @set status=1
 )
 
 :end
-@echo.
-@echo Build completed successfully.
-@echo.
+ECHO.
+IF "%status%" EQU "0" (
+  ECHO   [92m Build completed [1mSuccessfully! [0m [0m
+) ELSE (
+  ECHO   [91m Build was [1mUnsuccessfully :( [0m [0m
+)
+ECHO.
