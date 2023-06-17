04601b165dc862bffbf95e67b0cdf4866c74e9b5
Kara Rawson <rawsonkara@gmail.com>
2023-06-17 08:32:06 -0400

    remove main.cpp in project root

--- compiler/main.cpp
+++
@@ -1,9 +0,0 @@
-// compiler.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello ByteScript!";
-}
