38d242acb7fd1be0302bc595178be3b19267eb2d
Joe Pea <joe@trusktr.io>
2023-06-19 01:33:04 -0700

    Switch to Playwright for web-test-runner browser tests. We run Chromium, Firefox, and Webkit in Linux and macOS, and in Windows we run only Chromium and Firefox because Webkit has no WebAssembly in Windows. We currently cannot run Edge in any OS (https://github.com/microsoft/playwright/issues/23783).

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,5 +1,17 @@
+import os from 'os'
+import {playwrightLauncher} from '@web/test-runner-playwright'
 import {esbuildPlugin} from '@web/dev-server-esbuild'
 
 export default {
+	browsers: [
+		playwrightLauncher({product: 'chromium'}),
+		playwrightLauncher({product: 'firefox'}),
+
+		// Edge is not yet working, even in windows-latest. (https://github.com/microsoft/playwright/issues/23783)
+		// playwrightLauncher({product: 'msedge'}),
+
+		// Skip webkit in Windows, it doesn't have WebAssembly (https://github.com/microsoft/playwright/issues/2876)
+		...(os.platform().startsWith('win') ? [] : [playwrightLauncher({product: 'webkit'})]),
+	],
 	plugins: [esbuildPlugin({ts: true})],
 }
